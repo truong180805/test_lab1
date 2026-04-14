537264bf8e7685f4c1b1ad79a1256d215c1eaff2
truong180805 <nguyentruong180805@gmail.com>
2026-04-14 15:10:31 +0700

    Giải quyết Issue #2: Thêm các test case cho Path Coverage

--- src/test/java/org/yourcompany/yourproject/NumberAnalyzerTest.java
+++ src/test/java/org/yourcompany/yourproject/NumberAnalyzerTest.java
@@ -1,7 +1,7 @@
 package org.yourcompany.yourproject;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class NumberAnalyzerTest {
 
@@ -14,5 +14,21 @@ public class NumberAnalyzerTest {
         
         // Có 2 số dương là 5 và 10, nên kết quả mong đợi là 2
         assertEquals(2, analyzer.countPositiveNumbers(input));
+    }
+    
+    @Test
+    public void testPath_EmptyArray() {
+        NumberAnalyzer analyzer = new NumberAnalyzer();
+        // Đường đi 1: Mảng rỗng
+        int[] input = {}; 
+        assertEquals(0, analyzer.countPositiveNumbers(input));
+    }
+
+    @Test
+    public void testPath_AllNegative() {
+        NumberAnalyzer analyzer = new NumberAnalyzer();
+        // Đường đi 2: Mảng toàn số âm
+        int[] input = {-2, -4, -6}; 
+        assertEquals(0, analyzer.countPositiveNumbers(input));
     }
 }
